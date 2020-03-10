c6af62ddcacceefa3a151f147796bcdfe90eb1c1
shibata yasunobu <shibayasu13@gmail.com>
2020-03-10 06:13:55 +0000

    deploy setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, uniqueness: true, length: {minimum: 2, maximum: 20 }
-  validates :introduction, length: { maximum: 50 }
+  validates :introduction, length: { minimum: 1, maximum: 50 }
 
   has_many :books,dependent: :destroy
   attachment :profile_image
